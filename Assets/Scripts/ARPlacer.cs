0f2ff4295bd7f097e730bc4607c415ac02750bde
Kiharson <kiharabhi@gmail.com>
2021-10-15 08:22:02 +0530

    added files

---
+++ Assets/Scripts/ARPlacer.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.XR;
+using UnityEngine.XR.ARFoundation;
+
+public class ARPlacer : MonoBehaviour
+{
+    ARRaycastManager m_RaycastManager;
+
+    List<ARRaycastHit> raycast_hits = new List<ARRaycastHit>();
+
+    //this is the prefab to be instantiated
+    public GameObject aRObjectPrefab;
+
+    //this is the gameobject that is intantiated after a successfull raycast intersection with a plane
+    private GameObject spawnedARObject;
+
+    private void Awake()
+    {
+        m_RaycastManager = GetComponent<ARRaycastManager>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+
+            if (m_RaycastManager.Raycast(touch.position,raycast_hits,UnityEngine.XR.ARSubsystems.TrackableType.PlaneWithinPolygon))
+            {
+                //Getting Pose
+                Pose pose = raycast_hits[0].pose;
+
+
+                if (spawnedARObject==null)
+                {
+                    spawnedARObject = Instantiate(aRObjectPrefab,pose.position,Quaternion.Euler(0,0,0));
+
+                }else
+                {
+                    spawnedARObject.transform.position = pose.position;
+                }
+
+            }
+
+        }
+
+    }
+}
---
+++ Assets/Scripts/DroneMover.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mapbox.Unity.Map;
+using Mapbox.Utils;
+using UnityEngine.UI;
+
+public class DroneMover : MonoBehaviour
+{
+    [SerializeField]
+    AbstractMap _map;
+
+    public Vector2d railMuseum;
+    public Vector2d temple;
+    public Vector2d marblePalace;
+
+    bool shouldMove = false;
+    bool isArrived = false;
+    public float droneSpeed = 0.5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _map = FindObjectOfType<AbstractMap>();
+        shouldMove = true;
+
+        Button buttonRailMuseum = GameObject.Find("RailMuseumButton").GetComponent<Button>();
+        buttonRailMuseum.onClick.AddListener(() => { FlyToRailMuseum(); });
+
+        Button butttonTemple = GameObject.Find("TempleButton").GetComponent<Button>();
+        butttonTemple.onClick.AddListener(() => { FlyToTemple(); });
+
+        Button buttonMarblePalace = GameObject.Find("MarblePalaceButton").GetComponent<Button>();
+        buttonMarblePalace.onClick.AddListener(() => { FlyToMarblePalace(); }); 
+    }
+
+    public void FlyToRailMuseum()
+    {
+        StopAllCoroutines();
+        StartCoroutine(FlyToPoint(railMuseum));
+    }
+
+    public void FlyToTemple()
+    {
+        StopAllCoroutines();
+        StartCoroutine(FlyToPoint(temple));
+    }
+
+    public void FlyToMarblePalace()
+    {
+        StopAllCoroutines();
+        StartCoroutine(FlyToPoint(marblePalace));
+    }
+
+    IEnumerator FlyToPoint(Vector2d locationPoint)
+    {
+        Vector3 destination = _map.GeoToWorldPosition(locationPoint);
+
+        while(Vector3.Distance(transform.position, destination) > 0.01f)
+        {
+            var distanceCovered = droneSpeed * Time.deltaTime;
+            transform.position = Vector3.Lerp(transform.position, destination, distanceCovered);
+            yield return 0;
+        }
+
+    }
+}
